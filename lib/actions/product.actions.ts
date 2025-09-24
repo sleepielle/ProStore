1df09a1145568b540f50a70bf914ee0c80da2e00
sleepielle <pggisselle@gmail.com>
2025-09-24 22:27:31 +0200

    feat: product page with serverless env configuration from neon/prisma

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 "use server";
-//using server actions instead of api routes for mutations
-import { PrismaClient } from "@/generated/prisma";
-import { convertToPlainObject } from "../utils";
+//using server actions instead of api routes and methods
+
+//this links to the prisma database and we can write the methods to return our data
+import { prisma } from "@/db/prisma";
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   //fetch latest 4 products
   const data = await prisma.product.findMany({
     take: 4,
@@ -13,5 +12,12 @@ export async function getLatestProducts() {
   });
 
   //converts prisma object to plain object to avoid errors with types
-  return convertToPlainObject(data);
+  return data;
+}
+
+//get single product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
