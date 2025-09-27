36be233a0053102b1345574b7ab4ad59e6b99647
sleepielle <pggisselle@gmail.com>
2025-09-27 01:26:05 -0600

    fix: fix callbackurl route on deployment

--- auth.ts
+++ auth.ts
@@ -94,5 +94,29 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       return token;
     },
+    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
+      const isRelativeUrl = url.startsWith("/");
+      if (isRelativeUrl) {
+        return `${baseUrl}${url}`;
+      }
+
+      const isSameOriginUrl = new URL(url).origin === baseUrl;
+      const alreadyRedirected = url.includes("callbackUrl=");
+      if (isSameOriginUrl && alreadyRedirected) {
+        const originalCallbackUrl = decodeURIComponent(
+          url.split("callbackUrl=")[1]
+        );
+
+        return originalCallbackUrl;
+      }
+
+      if (isSameOriginUrl) {
+        return url;
+      }
+
+      return baseUrl;
+    },
   },
 });
+
+// ? why did moving next-auth.d.ts fix the next-auth has no exported member NextAuthConfig?
